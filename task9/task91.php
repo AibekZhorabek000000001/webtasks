2af44d92f4250df601f32c8a47e0dd1ddccfd113
AibekZhorabek000000001 <180107091@stu.sdu.edu.kz>
2019-11-14 14:12:12 +0600

    Add files via upload

---
+++ task9/task91.php
@@ -0,0 +1,34 @@
+
+<a href="task91.php?category=sport">Sport news</a> | 
+<a href="task91.php?category=politics">Politic news</a>
+<br/><br/>
+<a href="task91.php?category=sport&format=json">Sport news (JSON)</a> | 
+<a href="task91.php?category=politics&format=json">Politic news (JSON)</a><br/><br/>
+
+<?php
+$news = ["sport"=>["C. Ronaldo has scored three goals in last five matches","Golovkin has won match for title"],"politics"=>["Trump has cancelled his visit to North Corea, because of sanction","N. Nazarbayev has approved new version of alphabet"]];
+$category = ["sport", "politics"];
+
+/* Write your code here */
+
+if(isset($_REQUEST['format'])){
+    if($_REQUEST['category']==$category[0]){
+        echo json_encode($news[$category[0]]);
+    }
+    if($_REQUEST['category']==$category[1]){
+        echo json_encode($news[$category[1]]);
+    }
+}
+elseif($_REQUEST['category']){
+    if($_REQUEST['category']==$category[0]){
+        foreach($news[$category[0]] as $x){
+            echo $x ."<hr>";
+        }
+    }
+    if($_REQUEST['category']==$category[1]){
+        foreach($news[$category[1]] as $x){
+            echo $x ."<hr>";
+        }
+    }
+}
+?>
